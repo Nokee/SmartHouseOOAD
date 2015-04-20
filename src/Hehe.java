aed9be49c6c66e9edca16e7745d4b547c5ecf4ac
Nokee <kenobaz@hotmail.com>
2015-04-20 17:01:34 +0200

    Swift-JFrame

---
+++ src/Hehe.java
@@ -0,0 +1,68 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class Hehe extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField text;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Hehe frame = new Hehe();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Hehe() {
+		setTitle("Pokusaj");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnPritisniMe = new JButton("Pritisni me");
+		btnPritisniMe.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				text.setText("Hvala sto ste me pritisnuli");
+			}
+		});
+		btnPritisniMe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnPritisniMe.setBounds(171, 88, 89, 23);
+		contentPane.add(btnPritisniMe);
+		
+		text = new JTextField();
+		text.setEnabled(false);
+		text.setBounds(83, 156, 256, 20);
+		contentPane.add(text);
+		text.setColumns(10);
+	}
+}
