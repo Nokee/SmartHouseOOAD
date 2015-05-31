4584e021e63488592b81ca459990c7fdd4867d78
Nokee <kenobaz@hotmail.com>
2015-05-31 22:42:06 +0200

    II

--- src/Uredjaji.java
+++ src/Uredjaji.java
@@ -1,4 +1,31 @@
-
 public class Uredjaji {
+	
+	
+	/**
+	 * Salje mikrokontroleru o ukljucivanju uredjaja
+	 */
+	public void ukljuciUredjaj()
+	{
+		//Send();
+	}
+
+	/**
+	 * Salje mikrokontroleru o iskljucivanju uredjaja
+	 */
+	public void iskljuciUredjaj()
+	{
+		//Send();
+	}
+	/**
+	 * Zadaje se naredba koja se proslijedi uredjaju
+	 * @param u
+	 */
+	//TODO
+	public void postaviUredjaj(UredjajController u)
+	{
+		
+	};
+	
+	
 
 }
